77d9cc635238837f598c743d8a299b707baf3339
Garrett <gphelps@agleader.com>
2017-11-14 13:10:06 -0600

    Refactor DB location for played cards

--- src/components/PlayedCards.vue
+++ src/components/PlayedCards.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="players-container">
     <div class="player-container" v-for="player in players" :key="player.id">
-        <div class="card-container" v-for="card in player.cards" :key="card.id">
+        <div class="card-container" v-for="card in player.played" :key="card.id">
             <WhiteCard v-bind:class="{ revealed: card.revealed }" 
                 @click.native="card.revealed = !card.revealed"
                 :text="RevealedText(card)"
@@ -36,50 +36,26 @@ export default {
     },
     watch: {
         game: function (newVal) {
-            this.$bindAsArray('players', DB.ref(this.game + "/cards_played"))
-        }
-    },
-    firebase: {
-        players: {
-            source: DB.ref(this.game + "/cards_played")
+            this.$bindAsArray('players', DB.ref(this.game + "/players"))
         }
     },
     methods: {
         UpdatePlayed(player, white_card) {
-            let player_idx = -1;
-            let db_key = -1;
-            for (let i = 0; i < this.players.length; i++) {
-                let element = this.players[i];
-                if ( element.name == player ) {
-                    db_key = element[".key"];
-                    player_idx = i;
-                    break;
-                }
-            } 
-
-            if (player_idx < 0 ) {
-                let obj_val = {
-                    id: ++playerId,
-                    name: player,
-                    cards: [{ id: ++cardId, text: white_card, revealed: false }]
-                }
-                this.$firebaseRefs.players.push(obj_val).getKey();
-            } else {
-                let cards_obj = { 
-                    id: ++cardId, 
-                    text: white_card, 
-                    revealed: false 
-                }
-
-                console.log(this.$firebaseRefs.players.child(db_key));
-                this.$firebaseRefs.players.child(db_key).child('cards').push(cards_obj);
+            let cards_obj = { 
+                id: ++cardId, 
+                text: white_card, 
+                revealed: false 
             }
+            this.$firebaseRefs.players.child(player).child('played').push(cards_obj);
         },
         RevealedText: function(card) {
             return card.revealed ? card.text : this.hiddenText;
         },
         Clear() {
-            this.$firebaseRefs.players.remove();
+            for (let i = 0; i < this.players.length; i++) {
+                var element = this.players[i];
+                this.$firebaseRefs.players.child(element[".key"]).child("played").remove();
+            } 
         }
     }
 };
